9e4da1a67f2b17b3d5bab9d874347b5b82f0ddbc
jordanmosongo <mosongojordan@gmail.com>
2021-05-06 11:32:49 +0200

    gestion des etats et effets de la barre de menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,27 @@ const Header = (props) => {
   const showOrHideMenu = () => {
     setmenuState(!menuState);
   };
+  const manageBackground = (point) => {
+    if (point < 120) {
+      setaddBackground(false);
+    } else if (point > 120) {
+      setaddBackground(true);
+    }
+  };
+  const manageSearch = (point) => {
+    if (point < 490) {
+      setshowSearch(false);
+    } else if (point > 490) {
+      setshowSearch(true);
+    }
+  };
+  window.onscroll = () => {
+    let scrollPoint =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    manageBackground(scrollPoint);
+    manageSearch(scrollPoint);
+  };
+
   return (
     <>
       <div className={addBackground ? "header add-black" : "header"}>
