c565b8d68ad4ecb557b6f284ee332dc6a81049e4
jordanmosongo <mosongojordan@gmail.com>
2021-05-09 14:11:15 +0200

    feat:mise en place de top movies in theatres

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "../scss/collection.scss";
-import movieOne from "../images/movieOne.jpeg";
+import movieOne from "../images/filmFour.jpg";
+import movieTwo from "../images/filmOne.jpg";
+import movieThree from "../images/filmThree.jpg";
+import movieSecond from "../images/filmTwo.jpg";
+import Title from "./subcomponents/Title";
 
 const Collection = () => {
   return (
     <div className="collection">
       <div className="collection__container">
-        <div className="collection__container__text">
-          <p>
-            <span>Dans cette collection,</span> <br /> Retrouvez des films et
-            series ainsi que toutes les informations y relatives
-          </p>
-        </div>
+        <h2>Top movies in theatres</h2>
         <div className="collection__container__image">
-          <img src={movieOne} />
+          <div className="card-image">
+            <img src={movieOne} />
+          </div>
+          <div className="card-image">
+            <img src={movieTwo} />
+          </div>
+          <div className="card-image">
+            <img src={movieThree} />
+          </div>
+          <div className="card-image">
+            <img src={movieSecond} />
+          </div>
+          <div className="card-image">
+            <img src={movieOne} />
+          </div>
         </div>
       </div>
     </div>
