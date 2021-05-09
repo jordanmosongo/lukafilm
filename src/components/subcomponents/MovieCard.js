7bcea4ca0fb382ba3614d5109caf130de8e01ae8
jordanmosongo <mosongojordan@gmail.com>
2021-05-10 01:27:42 +0200

    style: ajout des effets

--- src/components/subcomponents/MovieCard.js
+++ src/components/subcomponents/MovieCard.js
@@ -1,13 +1,30 @@
 import { Favorite, People, Visibility } from "@material-ui/icons";
 import React from "react";
+import { useState } from "react";
 const linkImage = "https://image.tmdb.org/t/p/w300";
 const MovieCard = ({ movie }) => {
   //const { poster_path, title, isLatest, overview } = movie;
+  const [detail, setdetail] = useState(false);
   const showDetail = (event) => {
     event.preventDefault();
+    let strigger = event.target;
+    strigger.parentNode.classList.add("movie-card__effect");
+
+    if (strigger.parentNode.classList.contains("movie-card")) setdetail(true);
   };
+  const hideDetail = (event) => {
+    event.preventDefault();
+    let strigger = event.target;
+    strigger.parentNode.classList.remove("movie-card__effect");
+    setdetail(false);
+  };
+
   return (
-    <div className="movie-card" onMouseEnter={showDetail}>
+    <div
+      className="movie-card"
+      onMouseEnter={showDetail}
+      onMouseLeave={hideDetail}
+    >
       <div className="movie-card__image">
         {/* <img src={linkImage + poster_path} /> */}
       </div>
@@ -27,6 +44,16 @@ const MovieCard = ({ movie }) => {
         <Favorite className="favorite" />
         <span>250</span>
       </div>
+      {!detail ? (
+        ""
+      ) : (
+        <>
+          <div className="movie-card__over-effect"></div>
+          <div className="movie-card__over-content">
+            <button>View detail</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
